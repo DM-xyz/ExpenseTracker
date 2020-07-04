e3fd06aaa6dfbba3b3de827e18cf038bb2a3a821
dmorris <dm.morris@outlook.com>
2020-07-04 15:34:54 +0930

    Add stuff

---
+++ script.js
@@ -0,0 +1,78 @@
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const dummyTransactions = [
+    { id: 1, text: 'Flowers', amount: -20 },
+    { id: 2, text: 'Pay', amount: 500 },
+    { id: 3, text: 'Book', amount: -18 },
+    { id: 4, text: 'Camera', amount: 150 }
+];
+
+let transactions = dummyTransactions;
+
+function addTransaction(e) {
+    e.preventDefault();
+
+    if(text.value.trim() === '' || amount.value.trim() === '') {
+        alert('Please add an amount');
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: amount.value
+        }
+        console.log(transaction);
+    }
+}
+
+function generateID() {
+    return Math.floor(Math.random() * 100000000);
+}
+
+function addTransactionDOM(transaction) {
+    const sign = transaction.amount < 0 ? '-' : '+';
+    const item = document.createElement('li');
+
+    item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+    item.innerHTML = `
+        ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span>
+        <button class="delete-btn">x</button>
+    `;
+
+    list.appendChild(item);
+}
+
+function updateValues() {
+    const amounts = transactions.map(transactions => transactions.amount);
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+    const income = amounts
+                        .filter(item => item > 0)
+                        .reduce((acc, item) => (acc += item), 0)
+                        .toFixed(2);
+    
+    const expense = (amounts
+                        .filter(item => item < 0)
+                        .reduce((acc, item) => (acc += item), 0) * -1)
+                        .toFixed(2);
+
+    balance.innerText = `$${total}`;
+    money_plus.innerText = `$${income}`;
+    money_minus.innerText = `$${expense}`;
+}
+
+function init() {
+    list.innerHTML = '';
+
+    transactions.forEach(addTransactionDOM);
+    updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
